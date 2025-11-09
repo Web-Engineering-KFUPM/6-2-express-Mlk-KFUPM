cc5e91de5e7fe4c9027f6dd48967d5b7d3ba4a84
abdalmalekl <abdalmalekfs@gmail.com>
2025-11-09 11:44:31 +0300

    Finished

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -1,11 +1,25 @@
 import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
-
-{/*write code for cors*/}
-
-
-{/*write code to define routes*/}
-
-
-{/*write code to create server*/}
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+{
+  /*write code for cors*/
+}
+app.use(cors());
+{
+  /*write code to define routes*/
+}
+app.get("/", (req, res) => {
+  res.send("Welcome to the Quote Generator API");
+});
+{
+  /*write code to create server*/
+}
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
